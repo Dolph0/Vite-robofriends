fd1335bd4ce3edf7436fa9b21ba860450e09a7a6
Dolph0 <delfin.artcel@gmail.com>
2025-08-28 10:10:55 +0100

    implementing vite

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import 'tachyons'
+import Card from './Card'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <Card/>
+   <Card/>
+   <Card/>
+   <Card/>
+   <Card/>
+   <Card/>
+  </StrictMode>,
+)
