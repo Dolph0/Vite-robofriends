66402269a7bc5af135375255731e56905484cb09
Dolph0 <delfin.artcel@gmail.com>
2025-08-28 20:30:30 +0100

    Card component

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ import "tailwindcss";
 
 const Card = (props) => {
     return(
-        <div class="bg-green-200 inline-block rounded-lg p-4 transition-transform hover:scale-103 shadow-sm m-1 text-center">
+        <div class="200bg-green- inline-block rounded-lg p-4 transition-transform hover:scale-103 shadow-sm m-1 text-center">
             <img src={`https://robohash.org/${props.id}?200x200`} alt="robots"/>
             <div>
                 <h2>{props.name}</h2>
